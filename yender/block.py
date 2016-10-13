862fb41853432062d25112005b2edc56bb94dd7d
okdshin <kokuzen@gmail.com>
2016-10-13 12:25:05 +0900

    move block_to_triangles

--- yender/block.py
+++ yender/block.py
@@ -14,19 +14,3 @@ class Block:
         self.name = name
         self.movable = movable
 
-def block_to_triangles(block, pos):
-    '''Make triangles of a block(i.e. box or tile with position)
-
-    Args:
-        block (list of three floats): block to be converted
-        pos (list of three floats): block position
-
-    Returns:
-        tuple of triangles and colors
-    '''
-    assert isinstance(block, Block)
-    if block.block_type == "block":
-        triangles, colors = box_triangles(block.color)
-    elif block.block_type == "tile":
-        triangles, colors = tile_triangles(block.color)
-    return triangles+(pos[0], pos[1], 0), colors
