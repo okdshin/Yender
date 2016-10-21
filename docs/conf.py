8338b80534ee1bf6dfac59a85258f9838032564e
okdshin <kokuzen@gmail.com>
2016-10-21 22:19:19 +0900

    add links in sidebar

--- docs/conf.py
+++ docs/conf.py
@@ -352,3 +352,7 @@ texinfo_documents = [
 # If true, do not generate a @detailmenu in the "Top" node's menu.
 #
 # texinfo_no_detailmenu = False
+
+html_sidebars = {
+    '**': ['globaltoc.html', 'relations.html', 'sourcelink.html', 'searchbox.html'],
+}
